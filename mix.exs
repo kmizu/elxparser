8cd4a4b4b91483cc4467b2eb644b724353112b68
Kota Mizushima <mizukota@gmail.com>
2016-10-24 21:25:53 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule ElxParser.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps]
   end
 
@@ -17,16 +18,16 @@ defmodule ElxParser.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+      [# These are the default files included in the package
+        name: :elxparser,
+        files: ["lib", "test", "mix.exs", "config.exs", "README*", "LICENSE*"],
+        maintainers: ["Kota Mizushima"],
+        licenses: ["MIT License"],
+        links: %{"GitHub" => "https://github.com/kmizu/elxparser"}]
   end
 end
